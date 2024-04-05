2a27193cd2d3a3d1d5ef7c564a8fd91ff4191896
vikram061 <jbvikram@outlook.com>
2024-04-05 15:18:16 +0530

    second change

--- public/index.php
+++ public/index.php
@@ -1,4 +1,27 @@
 <?php 
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
-require basePath('views/home.view.php');
+use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+// Instatiate the Router
+$router = new Router();
+
+// Get routes
+$routes = require basePath('routes.php');
+
+// Get current URI and HTTP method
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+
+// Route the request
+$router->route($uri);
+
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vikram/sunflex-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '14d0a245ace7a7acb27073cdae1b2bb1058832ac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vikram/sunflex-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '14d0a245ace7a7acb27073cdae1b2bb1058832ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
